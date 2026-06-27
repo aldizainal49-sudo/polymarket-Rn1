a539c46e3b35a206cbcd735c41577cb69857a278
aldizainal49-sudo <aldizainal49@gmail.com>
2026-06-28 00:55:20 +0700

    Update paper_executor.rs

--- src/order/paper_executor.rs
+++ src/order/paper_executor.rs
@@ -21,3 +21,107 @@ pub struct PaperPosition {
     pub avg_price: Decimal,
     pub entry_value: Decimal,
 }
+
+#[derive(Debug, Clone)]
+pub struct PaperExecutor {
+    account: PaperAccount,
+    state_file: String,
+}
+
+impl PaperExecutor {
+    pub fn new(initial_balance: Decimal, state_file: &str) -> Self {
+        Self {
+            account: PaperAccount {
+                balance: initial_balance,
+                positions: HashMap::new(),
+                trade_count: 0
+            },
+            state_file: state_file.to_string()
+        }
+    }
+
+    pub fn load_state(&mut self) -> Result<(), anyhow::Error> {
+        if let Ok(contents) = std::fs::read_to_string(&self.state_file) {
+            if let Ok(account) = serde_json::from_str(&contents) {
+                self.account = account;
+            }
+        }
+        Ok(())
+    }
+
+    fn save_state(&self) {
+        if let Ok(json) = serde_json::to_string_pretty(&self.account) {
+            let _ = std::fs::write(&self.state_file, json);
+        }
+    }
+
+    pub fn buy(&mut self, market_id: &str, token_id: &str, side: OrderSide, price: Decimal, shares: Decimal) -> Result<(), String> {
+        let cost = price * shares;
+        if cost > self.account.balance {
+            return Err(format!("Saldo tidak cukup: butuh ${:.2}, tersisa ${:.2}", cost, self.account.balance));
+        }
+        self.account.balance -= cost;
+        let side_str = match side {
+            OrderSide::Buy => "YES",
+            OrderSide::Sell => "NO"
+        };
+
+        let key = format!("{}_{}_{}", market_id, token_id, side_str);
+
+        if let Some(pos) = self.account.positions.get_mut(&key) {
+            let total_cost = pos.avg_price * pos.shares + cost;
+            pos.shares += shares;
+            pos.avg_price = total_cost / pos.shares;
+            pos.entry_value = pos.avg_price * pos.shares;
+        } else {
+            self.account.positions.insert(key, PaperPosition {
+                market_id: market_id.to_string(),
+                token_id: token_id.to_string(),
+                side: side_str.to_string(),
+                shares,
+                avg_price: price,
+                entry_value: cost
+            });
+        }
+        self.account.trade_count += 1;
+        info!("[PAPER] BUY {} | {} shares @ {:.2}", market_id, shares, price * Decimal::from(100));
+        self.save_state();
+        Ok(())
+    }
+
+    pub fn sell(&mut self, market_id: &str, token_id: &str, side: OrderSide, price: Decimal, shares: Decimal) -> Result<(), String> {
+        let side_str = match side {
+            OrderSide::Buy => "YES",
+            OrderSide::Sell => "NO"
+        };
+        let key = format!("{}_{}_{}", market_id, token_id, side_str);
+        let pos = self.account.positions.get_mut(&key).ok_or("Posisi tidak ditemukan")?;
+
+        if pos.shares < shares {
+            return Err("Shares tidak mencukupi".to_string());
+        }
+        pos.shares -= shares;
+        let revenue = price * shares;
+        self.account.balance += revenue;
+
+        if pos.shares == Decimal::ZERO {
+            self.account.positions.remove(&key);
+        } else {
+            pos.entry_value = pos.avg_price * pos.shares;
+        }
+        self.save_state();
+        Ok(())
+    }
+
+    pub fn get_balance(&self) -> Decimal {
+        self.account.balance
+    }
+
+    pub fn get_positions(&self) -> &HashMap<String, PaperPosition> {
+        &self.account.positions
+    }
+
+    pub fn get_trade_count(&self) -> u64 {
+        self.account.trade_count
+    }
+}
